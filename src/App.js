b1bc87321b97bbd70e2c95561e3ae43f4a702ff6
Harika Chowdary <harikachowdary@Harikas-MacBook-Air.local>
2019-07-24 20:36:01 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         How you doin 
+         I was changed on feature
         </a>
       </header>
     </div>
